5cf15805f6b1aa8b093ff2988ce3b753bc6215c4
kiranp035 <kiran.reddy1992@gmail.com>
2018-02-05 12:31:11 -0800

    updated main.js

--- main.js
+++ main.js
@@ -2,6 +2,6 @@
 <html>
 <body>
 <h2> what can javascript do? </h2>
-<p> Javascript can change the style of an html element.</p>
+<p> I think, Javascript can change the style of an html element.</p>
 </body>
 </html>
